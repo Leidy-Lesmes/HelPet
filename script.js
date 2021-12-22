01ebea37ad9bee508be21ed858a589cda505755f
Leidy-Lesmes <70681138+Leidy-Lesmes@users.noreply.github.com>
2021-12-21 23:36:06 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 console.log=("Script loaded!")
 var form1 = document.getElementById("form-1"); 
-var array =[]
+var name = document.getElementById("name");
+
+function writeFile (data){
+		const dive = document.getElementById()
+		var text = '<div class = "card" p-3>' + '<h3>' + data.name+ '<h3>' + 'div>'
+		div.innerHTML += text
+	}
+
+	const list = []
+
+
 form1.addEventListener("submit", function(event){
 	var name = form1.elements["name"].value
 	event.preventDefault();
-	const data = Object.fromEntries(new FormData(event.target).entries());
-	console.log(event);
-	console.log("submit");
-	console.log(data.name);
-	content.innerHTML = "aaaa";
-	content.innerHTML = data.name +  "nombre";
+	list.push(form1.elements['name'].value)
+	form1.elements['name'].value =""
+
+	localStorage.setItem('users', list)
+	console.log(list)
 
 });
